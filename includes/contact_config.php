786092d2ff2f5d3b4e23f9722c28c75045b289e0
mecachisenros <popi840@gmail.com>
2016-07-12 17:07:56 +0100

    Cleanup

--- includes/contact_config.php
+++ includes/contact_config.php
@@ -14,6 +14,13 @@ $contactSubTypeResult = civicrm_api3( 'ContactType', 'get', array(
 
 ?>
 
+<div class="caldera-config-group caldera-config-group-full">
+    <div class="caldera-config-field">
+        <label><input id="auto_pop" type="checkbox" name="{{_name}}[auto_pop]" value="1" {{#if auto_pop}}checked="checked"{{/if}}>Auto populate contact data with CiviCRM data if user logged in. (only for the first contact processor)</label>
+    </div>
+</div>
+<hr style="clear: both;" />
+
 <!-- Contact Link -->
 <h2>Contact Link</h2>
 <div id="contact_link" class="caldera-config-group">
@@ -52,65 +59,44 @@ $contactSubTypeResult = civicrm_api3( 'ContactType', 'get', array(
 <hr/>
 
 <!-- Contact Fields -->
-<!-- <button id="contact_fields_btn" class="button fields-btn" type="button" style="clear: both;">Show Standard fields</button>
-<div id="contact_fields_container" style="clear: both;"> -->
-    <h2 style="display: inline-block;">Standard Fields</h2>
-    <!-- <button id="clear_fields" style="float: right;" type="button">Clear fields</button> -->
-    <?php
-        $contactFieldsResult = civicrm_api3( 'Contact', 'getfields', array(
-            'sequential' => 1,
-        ));
+<h2 style="display: inline-block;">Standard Fields</h2>
+<!-- <button id="clear_fields" style="float: right;" type="button">Clear fields</button> -->
+<?php
+    $contactFieldsResult = civicrm_api3( 'Contact', 'getfields', array(
+        'sequential' => 1,
+    ));
 
-        $contactFields = array();
-        foreach ( $contactFieldsResult['values'] as $key => $value ) {
-            if( in_array( $value['name'], CiviCRM_Caldera_Forms::$contact_fields ) ){
-                $contactFields[$value['name']] = $value['title'];
-            }
+    $contactFields = array();
+    foreach ( $contactFieldsResult['values'] as $key => $value ) {
+        if( in_array( $value['name'], CiviCRM_Caldera_Forms::$contact_fields ) ){
+            $contactFields[$value['name']] = $value['title'];
         }
-        unset( $contactFields['id'], $contactFields['contact_type'], $contactFields['contact_sub_type'] );
-        $contactFields = array_diff_key( $contactFields, CiviCRM_Caldera_Forms::get_contact_custom_fields() );
+    }
+    unset( $contactFields['id'], $contactFields['contact_type'], $contactFields['contact_sub_type'] );
+    $contactFields = array_diff_key( $contactFields, CiviCRM_Caldera_Forms::get_contact_custom_fields() );
 
-        foreach( $contactFields as $key => $value ) { ?>
-        <div id="<?php echo $key; ?>" class="caldera-config-group">
-            <label><?php echo __($value); ?> </label>
-            <div class="caldera-config-field">
-              <?php echo __('{{{_field slug="' . $key . '"}}}'); ?>
-            </div>
+    foreach( $contactFields as $key => $value ) { ?>
+    <div id="<?php echo $key; ?>" class="caldera-config-group">
+        <label><?php echo __($value); ?> </label>
+        <div class="caldera-config-field">
+          <?php echo __('{{{_field slug="' . $key . '"}}}'); ?>
         </div>
-    <?php } ?>
-    <hr style="clear: both;" />
-<!-- </div> -->
+    </div>
+<?php } ?>
+<hr style="clear: both;" />
 
 <!-- Contact Custom Fields -->
-<!-- <button id="custom_fields_btn" class="button fields-btn" type="button" style="clear: both;">Show Custom fields</button>
-<div id="custom_fields_container"> -->
-    <h2 style="display: inline-block;">Custom Fields</h2>
-    <!-- <button id="clear_custom_fields" style="float: right;" type="button">Clear fields</button> -->
-    <?php
-
-        $contactCustomFields = CiviCRM_Caldera_Forms::get_contact_custom_fields();
-
-        foreach( $contactCustomFields as $key => $value ) { ?>
-        <div id="<?php echo $key; ?>" class="caldera-config-group">
-            <label><?php echo __($value); ?> </label>
-            <div class="caldera-config-field">
-              <?php echo __('{{{_field slug="' . $key . '"}}}'); ?>
-            </div>
-        </div>
-    <?php } ?>
-<!-- </div> -->
+<h2 style="display: inline-block;">Custom Fields</h2>
+<!-- <button id="clear_custom_fields" style="float: right;" type="button">Clear fields</button> -->
 <?php
-/*
-if ( class_exists( 'Caldera_Forms_Processor_UI' ) ) {
 
-  //get the field  configuration
-  $cf_civicrm_contact_fields = cf_civicrm_contact_fields();
+    $contactCustomFields = CiviCRM_Caldera_Forms::get_contact_custom_fields();
 
-  //get HTML for UI
-  $config_fields = Caldera_Forms_Processor_UI::config_fields( $cf_civicrm_contact_fields );
-
-  //echo UI
-  echo $config_fields;
-}
-*/
-?>
+    foreach( $contactCustomFields as $key => $value ) { ?>
+    <div id="<?php echo $key; ?>" class="caldera-config-group">
+        <label><?php echo __($value); ?> </label>
+        <div class="caldera-config-field">
+          <?php echo __('{{{_field slug="' . $key . '"}}}'); ?>
+        </div>
+    </div>
+<?php } ?>
