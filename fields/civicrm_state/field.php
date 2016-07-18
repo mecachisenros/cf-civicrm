789ed5bb8f135553bb8c8fdc05e08e6d79a4fe02
Andrei Mondoc <popi840@gmail.com>
2016-07-18 22:20:13 +0100

    JS to limit State/Province options by Country

--- fields/civicrm_state/field.php
+++ fields/civicrm_state/field.php
@@ -6,7 +6,7 @@ echo $wrapper_before; ?>
 	<?php echo $field_label; ?>
 	<?php echo $field_before; ?>
 		<?php ob_start(); ?>
-		<select <?php echo $field_placeholder; ?> id="<?php echo $field_id; ?>" data-field="<?php echo $field_base_id; ?>" class="<?php echo $field_class; ?>" name="<?php echo $field_name; ?>" <?php echo $field_required; ?>>
+		<select <?php echo $field_placeholder; ?> id="<?php echo $field_id . '_cf_civicrm_state'; ?>" data-field="<?php echo $field_base_id; ?>" class="<?php echo $field_class; ?>" name="<?php echo $field_name; ?>" <?php echo $field_required; ?>>
 			
 			<?php if( empty( $field['config']['placeholder'] ) ){
 					echo '<option value="">' . ( !empty($field['hide_label']) ? $field['label'] : null ) . '</option>';
@@ -19,7 +19,7 @@ echo $wrapper_before; ?>
 				}
 
 			foreach ($state as $key=>$value) {  
-				echo '<option value="' . $key . '">' . $value . '</option>';
+				echo '<option value="' . $key . '" data-crm-country-id="' . $value['country_id'] . '">' . $value . '</option>';
 
 			 } ?>
 		</select>
@@ -30,6 +30,21 @@ echo $wrapper_before; ?>
 			}
 			echo $states;
 		?>
+		
+		<script type="text/javascript">
+			jQuery( document ).ready( function(){
+				var cfCountries = jQuery('select[id*="_cf_civicrm_country"]');
+				var cfStates = jQuery('select[id*="_cf_civicrm_state"]');
+				cfCountries.change( function(){
+					if ( jQuery( this ).data( 'options' ) == undefined ){
+						jQuery( this ).data( 'options', jQuery('select[id*="_cf_civicrm_state"] option').clone() );
+					}
+					var id = jQuery( this ).val();
+					var options = jQuery( this ).data( 'options' ).filter( '[data-crm-country-id=' + id + ']' );
+					cfStates.html( options );
+				});
+			});
+		</script>
 
 		<?php echo $field_caption; ?>
 	<?php echo $field_after; ?>
