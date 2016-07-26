6d458360df2be4a79b559ea116a405b722a5191a
Andrei Mondoc <andreimondoc@gmail.com>
2016-07-26 09:48:14 +0100

    Added default Country option in field config #3

--- fields/civicrm_country/config.php
+++ fields/civicrm_country/config.php
@@ -4,3 +4,20 @@
 		<input type="text" id="{{_id}}_placeholder" class="block-input field-config" name="{{_name}}[placeholder]" value="{{placeholder}}">
 	</div>
 </div>
+<div class="caldera-config-group">
+	<label><?php _e('Default'); ?></label>
+	<div class="caldera-config-field">
+		<select id="{{_id}}_default" class="block-input field-config" name="{{_name}}[default]" value="{{default}}">
+        <option value="" {{#is default value=""}}selected="selected"{{/is}}></option>
+        <?php 
+        $country = civicrm_api3('Country', 'get', array(
+    		'sequential' => 1,
+    		'return' => array("id", "name"),
+    		'options' => array('limit' => 0),
+		));
+        foreach( $country['values'] as $key => $value) { ?>
+            <option value="<?php echo $value['id']; ?>" {{#is default value=<?php echo $value['id']; ?>}}selected="selected"{{/is}}><?php echo $value['name']; ?></option>
+        <?php } ?>
+        </select>
+	</div>
+</div>
