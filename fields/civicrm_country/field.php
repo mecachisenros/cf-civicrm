82dd72c0deadd74dccf4e00ca0e1b67d491a8a66
Andrei Mondoc <andreimondoc@gmail.com>
2016-07-29 16:42:42 +0100

    Revert "Limit Country and State/Province fields to CiviCRM's localization settings"
    This reverts commit 1acfd2d2485f7d4cccf5cea94c206c4296501077.

--- fields/civicrm_country/field.php
+++ fields/civicrm_country/field.php
@@ -1,9 +1,8 @@
-<?php
+<?php 
 
 $country = civicrm_api3('Country', 'get', array(
     'sequential' => 1,
     'options' => array('limit' => 0),
-    'iso_code' => array('IN' => CRM_Core_BAO_Country::countryLimit()),
 ));
 
 echo $wrapper_before; ?>
@@ -11,7 +10,7 @@ echo $wrapper_before; ?>
 	<?php echo $field_before; ?>
 		<?php ob_start(); ?>
 		<select <?php echo $field_placeholder; ?> id="<?php echo $field_id . '_cf_civicrm_country'; ?>" data-field="<?php echo $field_base_id; ?>" class="<?php echo $field_class; ?>" name="<?php echo $field_name; ?>" <?php echo $field_required; ?>>
-
+			
 			<?php if( empty( $field['config']['placeholder'] ) ){
 					echo '<option value="">' . ( !empty($field['hide_label']) ? $field['label'] : null ) . '</option>';
 				}else{
@@ -22,7 +21,7 @@ echo $wrapper_before; ?>
 					echo '<option value="" disabled ' . $sel . '>' . $field['config']['placeholder'] . '</option>';
 				}
 
-			foreach ($country['values'] as $key=>$value) {
+			foreach ($country['values'] as $key=>$value) {  
 				echo '<option value="' . $value['id'] . '">' . $value['name'] . '</option>';
 
 			 } ?>
