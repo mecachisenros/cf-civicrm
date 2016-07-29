1acfd2d2485f7d4cccf5cea94c206c4296501077
Andrei Mondoc <andreimondoc@gmail.com>
2016-07-29 16:31:08 +0100

    Limit Country and State/Province fields to CiviCRM's localization settings
    Countries and States/Provinces are limited to the "Available Country"
    and "Available States and Provinces" in the Localization settings

--- fields/civicrm_country/config.php
+++ fields/civicrm_country/config.php
@@ -14,6 +14,7 @@
     				'sequential' => 1,
     				'return' => array("id", "name"),
     				'options' => array('limit' => 0),
+					'iso_code' => array('IN' => CRM_Core_BAO_Country::countryLimit()),
 				));
         foreach( $country['values'] as $key => $value) { ?>
             <option value="<?php echo $value['id']; ?>"><?php echo $value['name']; ?></option>
