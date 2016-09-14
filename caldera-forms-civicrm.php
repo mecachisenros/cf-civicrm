679d88eb296de853918af0bd9832beb7d7866c7f
Christian Wach <needle@haystack.co.uk>
2016-09-14 18:10:02 +0100

    add basic readme.txt and rename main plugin file

--- caldera-forms-civicrm.php
+++ caldera-forms-civicrm.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: Caldera Forms - CiviCRM Integration
- * Description: Civicrm Integration for Caldera Forms, requieres Cladera Forms and CiviCRM
+ * Plugin Name: Caldera Forms CiviCRM
+ * Description: CiviCRM integration for Caldera Forms.
  * Version: 0.1.1
  * Author: Andrei Mondoc
  */
@@ -18,13 +18,13 @@ define( 'CF_CIVICRM_INTEGRATION_CORE',    dirname( __FILE__ )  );
 add_action('init', 'cf_civicrm_integration_init');
 
 function cf_civicrm_integration_init(){
-	
+
 	// Include plugin functions
 	include CF_CIVICRM_INTEGRATION_PATH . 'includes/functions.php';
 
 	// Hook to register CiviCRM Integration add-on
 	add_filter( 'caldera_forms_get_form_processors', 'cf_civicrm_register_processor' );
-	
+
 	// FIXME
 	// Add example forms
 	// add_filter( 'caldera_forms_get_form_templates', 'cf_civicrm_template_examples' );
