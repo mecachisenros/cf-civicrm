8ef5e0f2cad8388121571c489f6599b8fad7c92c
Christian Wach <needle@haystack.co.uk>
2016-09-15 18:29:01 +0100

    sanitise fields and make strings translatable

--- fields/civicrm_state/config.php
+++ fields/civicrm_state/config.php
@@ -1,18 +1,18 @@
 <div class="caldera-config-group">
-	<label><?php echo 'Placeholder'; ?></label>
+	<label><?php _e( 'Placeholder', 'caldera-forms-civicrm' ); ?></label>
 	<div class="caldera-config-field">
 		<input type="text" id="{{_id}}_placeholder" class="block-input field-config" name="{{_name}}[placeholder]" value="{{placeholder}}">
 	</div>
 </div>
 <div class="caldera-config-group">
-    <label><?php _e('Default'); ?></label>
+    <label><?php _e( 'Default', 'caldera-forms-civicrm' ); ?></label>
     <div class="caldera-config-field">
         <select id="{{_id}}_default" class="block-input field-config" name="{{_name}}[default]" value="{{default}}">
         <option value="" {{#is default value=""}}selected="selected"{{/is}}></option>
         <?php
         $state = CiviCRM_Caldera_Forms_Helper::get_state_province();
-        foreach( $state as $key => $value) { ?>
-            <option value="<?php echo $key; ?>"><?php echo $value['name']; ?></option>
+        foreach( $state as $key => $value ) { ?>
+            <option value="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $value['name'] ); ?></option>
         <?php } ?>
         </select>
     </div>
