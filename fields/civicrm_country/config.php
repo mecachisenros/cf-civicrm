20d522791fa5b0deb4db9aa5aa9b0857a4d98580
Christian Wach <needle@haystack.co.uk>
2016-09-15 18:29:01 +0100

    sanitise fields and make strings translatable

--- fields/civicrm_country/config.php
+++ fields/civicrm_country/config.php
@@ -1,23 +1,25 @@
 <div class="caldera-config-group">
-	<label><?php echo 'Placeholder'; ?></label>
+	<label><?php _e( 'Placeholder', 'caldera-forms-civicrm' ); ?></label>
 	<div class="caldera-config-field">
 		<input type="text" id="{{_id}}_placeholder" class="block-input field-config" name="{{_name}}[placeholder]" value="{{placeholder}}">
 	</div>
 </div>
 <div class="caldera-config-group">
-	<label><?php _e('Default'); ?></label>
+    <label><?php _e( 'Default', 'caldera-forms-civicrm' ); ?></label>
 	<div class="caldera-config-field">
 		<select id="{{_id}}_default" class="block-input field-config" name="{{_name}}[default]" value="{{default}}">
         <option value="" {{#is default value=""}}selected="selected"{{/is}}></option>
         <?php
-        $country = civicrm_api3('Country', 'get', array(
-    				'sequential' => 1,
-    				'return' => array("id", "name"),
-    				'options' => array('limit' => 0),
-					'id' => array('IN' => CiviCRM_Caldera_Forms_Helper::get_civicrm_settings('countryLimit')),
-				));
-        foreach( $country['values'] as $key => $value) { ?>
-            <option value="<?php echo $value['id']; ?>"><?php echo $value['name']; ?></option>
+
+        $country = civicrm_api3( 'Country', 'get', array(
+			'sequential' => 1,
+			'return' => array( 'id', 'name' ),
+			'options' => array( 'limit' => 0 ),
+			'id' => array( 'IN' => CiviCRM_Caldera_Forms_Helper::get_civicrm_settings( 'countryLimit' ) ),
+		));
+
+        foreach( $country['values'] as $key => $value ) { ?>
+            <option value="<?php echo esc_attr( $value['id'] ); ?>"><?php echo esc_html( $value['name'] ); ?></option>
         <?php } ?>
         </select>
 	</div>
--- fields/civicrm_state/field.php
+++ fields/civicrm_state/field.php
@@ -6,43 +6,46 @@ echo $wrapper_before; ?>
 	<?php echo $field_label; ?>
 	<?php echo $field_before; ?>
 		<?php ob_start(); ?>
-		<select <?php echo $field_placeholder; ?> id="<?php echo $field_id . '_cf_civicrm_state'; ?>" data-field="<?php echo $field_base_id; ?>" class="<?php echo $field_class; ?>" name="<?php echo $field_name; ?>" <?php echo $field_required; ?>>
+		<select <?php echo $field_placeholder; ?> id="<?php echo esc_attr( $field_id . '_cf_civicrm_state' ); ?>" data-field="<?php echo esc_attr( $field_base_id ); ?>" class="<?php echo esc_attr( $field_class ); ?>" name="<?php echo esc_attr( $field_name ); ?>" <?php echo $field_required; ?>>
+			<?php
 
-			<?php if( empty( $field['config']['placeholder'] ) ){
-					echo '<option value="">' . ( !empty($field['hide_label']) ? $field['label'] : null ) . '</option>';
-				}else{
-					$sel = '';
-					if( empty( $field_value ) ){
-						$sel = 'selected';
-					}
-					echo '<option value="" disabled ' . $sel . '>' . $field['config']['placeholder'] . '</option>';
+			if ( empty( $field['config']['placeholder'] ) ) {
+				echo '<option value="">' . ( ! empty( $field['hide_label'] ) ? esc_html( $field['label'] ) : null ) . '</option>';
+			} else {
+				$sel = '';
+				if( empty( $field_value ) ) {
+					$sel = 'selected';
 				}
+				echo '<option value="" disabled ' . $sel . '>' . esc_html( $field['config']['placeholder'] ) . '</option>';
+			}
 
-			foreach ($state as $key=>$value) {
-				echo '<option value="' . $key . '" data-crm-country-id="' . $value['country_id'] . '">' . $value['name'] . '</option>';
+			foreach( $state as $key => $value ) {
+				echo '<option value="' . esc_attr( $key ) . '" data-crm-country-id="' . esc_attr( $value['country_id'] ) . '">' . esc_html( $value['name'] ) . '</option>';
 
 			 } ?>
 		</select>
 		<?php
-			$states = ob_get_clean();
-			if( !empty( $field_value ) ){
-				$states = str_replace( 'value="' . $field_value . '"', 'value="' . $field_value . '" selected="selected"', $states );
-			}
-			echo $states;
+
+		$states = ob_get_clean();
+		if( ! empty( $field_value ) ) {
+			$states = str_replace( 'value="' . $field_value . '"', 'value="' . $field_value . '" selected="selected"', $states );
+		}
+		echo $states;
+
 		?>
 
 		<script type="text/javascript">
-			jQuery( document ).ready( function(){
+			jQuery(document).ready( function() {
 				var cfCountries = jQuery('select[id*="_cf_civicrm_country"]');
 				var cfStates = jQuery('select[id*="_cf_civicrm_state"]');
 
 				if ( cfCountries !== undefined ) {
 					cfCountries.change( function(){
-						if ( jQuery( this ).data( 'options' ) == undefined ){
-							jQuery( this ).data( 'options', jQuery('select[id*="_cf_civicrm_state"] option').clone() );
+						if ( jQuery(this).data( 'options' ) == undefined ){
+							jQuery(this).data( 'options', jQuery('select[id*="_cf_civicrm_state"] option').clone() );
 						}
-						var id = jQuery( this ).val();
-						var options = jQuery( this ).data( 'options' ).filter( '[data-crm-country-id="' + id + '"]' );
+						var id = jQuery(this).val();
+						var options = jQuery(this).data( 'options' ).filter( '[data-crm-country-id="' + id + '"]' );
 						cfStates.html( options );
 					});
 				}
