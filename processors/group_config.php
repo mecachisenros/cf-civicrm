5c2f123564338eda14cfe19cda1f003745867098
Christian Wach <needle@haystack.co.uk>
2016-09-15 18:28:27 +0100

    sanitise processors and make strings translatable

--- processors/group_config.php
+++ processors/group_config.php
@@ -1,30 +1,32 @@
 <?php
-  $groupsResult = civicrm_api3('Group', 'get', array(
-		'sequential' => 1,
-		'cache_date' => array('IS NULL' => 1),
-		'is_active' => 1,
-		'options' => array('limit' => 0),
-	));
+
+$groupsResult = civicrm_api3( 'Group', 'get', array(
+	'sequential' => 1,
+	'cache_date' => array('IS NULL' => 1),
+	'is_active' => 1,
+	'options' => array('limit' => 0),
+));
+
 ?>
 
-<h2>Contact Link</h2>
+<h2><?php _e( 'Contact Link', 'caldera-forms-civicrm' ); ?></h2>
 <div id="contact_link" class="caldera-config-group">
-    <label><?php echo __('Link to'); ?></label>
-    <div class="caldera-config-field">
-        <?php CiviCRM_Caldera_Forms_Helper::contact_link_field(); ?>
-        <p>Select to witch contact you want to link this processor to.</p>
-    </div>
+	<label><?php _e( 'Link to', 'caldera-forms-civicrm' ); ?></label>
+	<div class="caldera-config-field">
+		<?php CiviCRM_Caldera_Forms_Helper::contact_link_field() ?>
+		<p><?php _e( 'Select which contact you want to link this processor to.', 'caldera-forms-civicrm' ); ?></p>
+	</div>
 </div>
+
 <hr style="clear: both;" />
 
 <div class="caldera-config-group">
-    <label><?php echo __('Group'); ?></label>
-    <div class="caldera-config-field">
-        <select class="block-input field-config" name="{{_name}}[contact_group]">
-        <?php foreach( $groupsResult['values'] as $key => $value) { ?>
-            <option value="<?php echo $value['id']; ?>" {{#is contact_group value=<?php echo $value['id']; ?>}}selected="selected"{{/is}}><?php echo $value['title']; ?></option>
-
-        <?php } ?>
-        </select>
-    </div>
+	<label><?php _e('Group', 'caldera-forms-civicrm' ); ?></label>
+	<div class="caldera-config-field">
+		<select class="block-input field-config" name="{{_name}}[contact_group]">
+		<?php foreach( $groupsResult['values'] as $key => $value ) { ?>
+			<option value="<?php echo esc_attr( $value['id'] ); ?>" {{#is contact_group value=<?php echo $value['id']; ?>}}selected="selected"{{/is}}><?php echo esc_html( $value['title'] ); ?></option>
+		<?php } ?>
+		</select>
+	</div>
 </div>
--- processors/relationship_config.php
+++ processors/relationship_config.php
@@ -1,56 +1,54 @@
 <?php
 
-$relationships = civicrm_api3('RelationshipType', 'get', array(
-  'sequential' => 1,
+$relationships = civicrm_api3( 'RelationshipType', 'get', array(
+	'sequential' => 1,
 ));
 
 ?>
 
 <div id="relationship_type" class="caldera-config-group">
-    <label><?php echo __('Relationship Type'); ?></label>
-    <div class="caldera-config-field">
-        <select class="block-input field-config" name="{{_name}}[relationship_type]">
-        <?php foreach( $relationships['values'] as $key => $value) { ?>
-            <option value="<?php echo $value['id']; ?>" {{#is relationship_type value=<?php echo $value['id']; ?>}}selected="selected"{{/is}}><?php echo '[' . $value['contact_type_a'] . ']' . $value['label_a_b'] . ' - ['. $value['contact_type_b'] . ']' . $value['label_b_a']; ?></option>
-        <?php } ?>
-        </select>
-    </div>
+	<label><?php echo __( 'Relationship Type', 'caldera-forms-civicrm' ); ?></label>
+	<div class="caldera-config-field">
+		<select class="block-input field-config" name="{{_name}}[relationship_type]">
+		<?php foreach( $relationships['values'] as $key => $value ) { ?>
+			<option value="<?php echo esc_attr( $value['id'] _; ?>" {{#is relationship_type value=<?php echo $value['id']; ?>}}selected="selected"{{/is}}><?php echo esc_html( '[' . $value['contact_type_a'] . ']' . $value['label_a_b'] . ' - ['. $value['contact_type_b'] . ']' . $value['label_b_a'] ); ?></option>
+		<?php } ?>
+		</select>
+	</div>
 </div>
 
 <div id="contact_a" class="caldera-config-group">
-    <label><?php echo __('Contact A'); ?></label>
-    <div class="caldera-config-field">
-        <select class="block-input field-config" name="{{_name}}[contact_a]">
-            <option value="1" {{#is contact_a value=1}}selected="selected"{{/is}}>Contact 1</option>
-            <option value="2" {{#is contact_a value=2}}selected="selected"{{/is}}>Contact 2</option>
-            <option value="3" {{#is contact_a value=3}}selected="selected"{{/is}}>Contact 3</option>
-            <option value="4" {{#is contact_a value=4}}selected="selected"{{/is}}>Contact 4</option>
-            <option value="4" {{#is contact_a value=4}}selected="selected"{{/is}}>Contact 4</option>
-            <option value="5" {{#is contact_a value=5}}selected="selected"{{/is}}>Contact 5</option>
-            <option value="6" {{#is contact_a value=6}}selected="selected"{{/is}}>Contact 6</option>
-            <option value="7" {{#is contact_a value=7}}selected="selected"{{/is}}>Contact 7</option>
-            <option value="8" {{#is contact_a value=8}}selected="selected"{{/is}}>Contact 8</option>
-            <option value="9" {{#is contact_a value=9}}selected="selected"{{/is}}>Contact 9</option>
-            <option value="10" {{#is contact_a value=10}}selected="selected"{{/is}}>Contact 10</option>
-        </select>
-    </div>
+	<label><?php _e( 'Contact A', 'caldera-forms-civicrm' ); ?></label>
+	<div class="caldera-config-field">
+		<select class="block-input field-config" name="{{_name}}[contact_a]">
+			<option value="1" {{#is contact_a value=1}}selected="selected"{{/is}}><?php _e( 'Contact 1', 'caldera-forms-civicrm' ); ?></option>
+			<option value="2" {{#is contact_a value=2}}selected="selected"{{/is}}><?php _e( 'Contact 2', 'caldera-forms-civicrm' ); ?></option>
+			<option value="3" {{#is contact_a value=3}}selected="selected"{{/is}}><?php _e( 'Contact 3', 'caldera-forms-civicrm' ); ?></option>
+			<option value="4" {{#is contact_a value=4}}selected="selected"{{/is}}><?php _e( 'Contact 4', 'caldera-forms-civicrm' ); ?></option>
+			<option value="5" {{#is contact_a value=5}}selected="selected"{{/is}}><?php _e( 'Contact 5', 'caldera-forms-civicrm' ); ?></option>
+			<option value="6" {{#is contact_a value=6}}selected="selected"{{/is}}><?php _e( 'Contact 6', 'caldera-forms-civicrm' ); ?></option>
+			<option value="7" {{#is contact_a value=7}}selected="selected"{{/is}}><?php _e( 'Contact 7', 'caldera-forms-civicrm' ); ?></option>
+			<option value="8" {{#is contact_a value=8}}selected="selected"{{/is}}><?php _e( 'Contact 8', 'caldera-forms-civicrm' ); ?></option>
+			<option value="9" {{#is contact_a value=9}}selected="selected"{{/is}}><?php _e( 'Contact 9', 'caldera-forms-civicrm' ); ?></option>
+			<option value="10" {{#is contact_a value=10}}selected="selected"{{/is}}><?php _e( 'Contact 10', 'caldera-forms-civicrm' ); ?></option>
+		</select>
+	</div>
 </div>
 
 <div id="contact_b" class="caldera-config-group">
-    <label><?php echo __('Contact B'); ?></label>
-    <div class="caldera-config-field">
-        <select class="block-input field-config" name="{{_name}}[contact_b]">
-            <option value="1" {{#is contact_b value=1}}selected="selected"{{/is}}>Contact 1</option>
-            <option value="2" {{#is contact_b value=2}}selected="selected"{{/is}}>Contact 2</option>
-            <option value="3" {{#is contact_b value=3}}selected="selected"{{/is}}>Contact 3</option>
-            <option value="4" {{#is contact_b value=4}}selected="selected"{{/is}}>Contact 4</option>
-            <option value="4" {{#is contact_b value=4}}selected="selected"{{/is}}>Contact 4</option>
-            <option value="5" {{#is contact_b value=5}}selected="selected"{{/is}}>Contact 5</option>
-            <option value="6" {{#is contact_b value=6}}selected="selected"{{/is}}>Contact 6</option>
-            <option value="7" {{#is contact_b value=7}}selected="selected"{{/is}}>Contact 7</option>
-            <option value="8" {{#is contact_b value=8}}selected="selected"{{/is}}>Contact 8</option>
-            <option value="9" {{#is contact_b value=9}}selected="selected"{{/is}}>Contact 9</option>
-            <option value="10" {{#is contact_b value=10}}selected="selected"{{/is}}>Contact 10</option>
-        </select>
-    </div>
+	<label><?php _e( 'Contact B', 'caldera-forms-civicrm' ); ?></label>
+	<div class="caldera-config-field">
+		<select class="block-input field-config" name="{{_name}}[contact_b]">
+			<option value="1" {{#is contact_b value=1}}selected="selected"{{/is}}><?php _e( 'Contact 1', 'caldera-forms-civicrm' ); ?></option>
+			<option value="2" {{#is contact_b value=2}}selected="selected"{{/is}}><?php _e( 'Contact 2', 'caldera-forms-civicrm' ); ?></option>
+			<option value="3" {{#is contact_b value=3}}selected="selected"{{/is}}><?php _e( 'Contact 3', 'caldera-forms-civicrm' ); ?></option>
+			<option value="4" {{#is contact_b value=4}}selected="selected"{{/is}}><?php _e( 'Contact 4', 'caldera-forms-civicrm' ); ?></option>
+			<option value="5" {{#is contact_b value=5}}selected="selected"{{/is}}><?php _e( 'Contact 5', 'caldera-forms-civicrm' ); ?></option>
+			<option value="6" {{#is contact_b value=6}}selected="selected"{{/is}}><?php _e( 'Contact 6', 'caldera-forms-civicrm' ); ?></option>
+			<option value="7" {{#is contact_b value=7}}selected="selected"{{/is}}><?php _e( 'Contact 7', 'caldera-forms-civicrm' ); ?></option>
+			<option value="8" {{#is contact_b value=8}}selected="selected"{{/is}}><?php _e( 'Contact 8', 'caldera-forms-civicrm' ); ?></option>
+			<option value="9" {{#is contact_b value=9}}selected="selected"{{/is}}><?php _e( 'Contact 9', 'caldera-forms-civicrm' ); ?></option>
+			<option value="10" {{#is contact_b value=10}}selected="selected"{{/is}}><?php _e( 'Contact 10', 'caldera-forms-civicrm' ); ?></option>
+		</select>
+	</div>
 </div>
