013088718cf5a7de74162e34f45a03cd21251194
Andrei Mondoc <andreimondoc@gmail.com>
2018-07-20 11:35:27 +0100

    cleanup

--- processors/line-item/class-line-item-processor.php
+++ processors/line-item/class-line-item-processor.php
@@ -81,46 +81,7 @@ class CiviCRM_Caldera_Forms_Line_Item_Processor {
 	 * @param array $form Form configuration
 	 */
 	public function pre_processor( $config, $form, $processid ) {
-
-		// globalised transient object
-		global $transdata;
-		$this->plugin->slack( $form['processors'][$config['processor_id']] );
-
-        // $transient->line_items->$config['processor_id'] = 
-
-		// $line_items = $config['line_item'];
-		// unset( $config['line_item'] );
-
-		// foreach ( $line_items as $key => $line_item ) {
-		// 	if ( empty( $line_item['price_field_value'] ) && empty( $line_item['entity_table'] ) ) {
-		// 		unset( $line_items[$key] );
-		// 		continue;
-		// 	}
-		// 	$line_items[$key] = $line_item;
-
-		// }
-
-		// Get form values
-		// $form_values = $this->plugin->helper->map_fields_to_processor( $config, $form, $form_values );
-
-		// $form_values['financial_type_id'] = $config['financial_type_id'];
-		// $form_values['contribution_status_id'] = $config['contribution_status_id'];
-
-		// $form_values['receipt_date'] = date('YmdHis');
-
-		// if( ! isset( $form_values['source'] ) )
-		// 	$form_values['source'] = $form['name'];
-
-		// $form_values['contact_id'] = $transient->contacts->{$this->contact_link};
-		// $form_values['line_items'] = $this->build_line_item_data( $line_items, $form );
-		// unset( $form_values['line_item'] );
-
-		// try {
-		// 	$create_order = civicrm_api3( 'Order', 'create', $form_values );
-		// } catch ( CiviCRM_API3_Exception $e ) {
-		// 	$error = $e->getMessage() . '<br><br><pre' . $e->getTraceAsString() . '</pre>';
-		// 	return array( 'note' => $error, 'type' => 'error' );
-		// }
+		
 	}
 
 	public function processor( $config, $form, $processid ) {
