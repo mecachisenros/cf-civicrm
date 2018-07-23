360befc4b6d5744f6720a627b649a82168e4c4df
Andrei Mondoc <andreimondoc@gmail.com>
2018-07-23 13:02:08 +0100

    line items ui improvements

--- processors/order2/order_config.php
+++ processors/order2/order_config.php
@@ -1,14 +1,18 @@
 <?php
 
-$financial_types = civicrm_api3( 'FinancialType', 'get', array(
+$financial_types = civicrm_api3( 'FinancialType', 'get', [
 	'sequential' => 1,
 	'is_active' => 1,
-));
+] );
 
-$contribution_status = civicrm_api3( 'Contribution', 'getoptions', array(
+$contribution_status = civicrm_api3( 'Contribution', 'getoptions', [
   'sequential' => 1,
   'field' => 'contribution_status_id',
-));
+] );
+
+$payment_instruments = civicrm_api3( 'Contribution', 'getoptions', [
+	'field' => 'payment_instrument_id',
+] );
 
 $price_sets = caldera_forms_civicrm()->helper->get_price_sets();
 
@@ -53,11 +57,31 @@ $price_sets = caldera_forms_civicrm()->helper->get_price_sets();
 	</div>
 </div>
 
+<!-- Payment Method -->
+<div id="payment_instrument_id" class="caldera-config-group">
+	<label><?php _e( 'Payment Method', 'caldera-forms-civicrm' ); ?></label>
+	<div class="caldera-config-field">
+		<select class="block-input field-config" name="{{_name}}[payment_instrument_id]">
+		<?php foreach ( $payment_instruments['values'] as $id => $method ) { ?>
+			<option value="<?php echo esc_attr( $id ); ?>" {{#is payment_instrument_id value=<?php echo $id; ?>}}selected="selected"{{/is}}><?php echo esc_html( $method ); ?></option>
+		<?php } ?>
+		</select>
+	</div>
+</div>
+
 <!-- Recieve Date -->
 <div id="receive_date" class="caldera-config-group">
 	<label><?php _e( 'Receive Date', 'caldera-forms-civicrm' ); ?></label>
 	<div class="caldera-config-field">
 		{{{_field slug="receive_date"}}}
+	</div>
+</div>
+
+<!-- Transaction ID -->
+<div id="trxn_id" class="caldera-config-group">
+	<label><?php _e( 'Transaction ID', 'caldera-forms-civicrm' ); ?></label>
+	<div class="caldera-config-field">
+		{{{_field slug="trxn_id"}}}
 	</div>
 </div>
 
@@ -73,27 +97,99 @@ $price_sets = caldera_forms_civicrm()->helper->get_price_sets();
 <!-- Line Items -->
 <h1><?php _e( 'Line Items', 'caldera-forms-civicrm' ); ?></h1>
 
-<div id="{{_id}}_line_items">
-    <?php for ( $num = 1; $num < 11; $num++ ) { ?>
-    <div id="line-item-<?php esc_attr( $num ); ?>" class="line-item caldera-config-group">
-		<label><?php _e( 'Line Item ' . $num, 'caldera-forms-civicrm' ); ?></label>
-		<div class="caldera-config-field">
-			<input type="text" class="block-input field-config magic-tag-enabled caldera-field-bind" id="{{_id}}" name="{{_name}}[line_items][line_item_<?php echo $num; ?>]" value="{{<?php echo 'line_items/line_item_' . $num; ?>}}">
-		</div>
+<div class="caldera-config-group">
+	<div class="caldera-config-field">
+		<button 
+			id="{{_id}}_line_item_add" 
+			type="button" 
+			data-id="{{_id}}" 
+			data-complete="line_item_built" 
+			class="pull-right button ajax-trigger" 
+			data-template="#line-item-tmpl" 
+			data-target-insert="append" 
+			data-request="cfc_add_line_item" 
+			data-target="#{{_id}}_line_items_wrapper"><?php esc_html_e( 'Add Line Item', 'caldera-forms-civicrm' ); ?></button>
 	</div>
-	<?php } ?>
 </div>
 
+<div class="{{_id}}_line_items" id="{{_id}}_line_items_wrapper">
+    {{#each this.line_items as |value item|}}
+	    <div id="{{item}}" data-id="{{_id}}" class="line-item caldera-config-group">
+			<label><?php _e( 'Line Item', 'caldera-forms-civicrm' ); ?></label>
+			<div class="caldera-config-field">
+				<input type="text" class="block-input field-config magic-tag-enabled caldera-field-bind" name="{{../_name}}[line_items][{{item}}]" value="{{value}}">
+				<button type="button" class="button remove-line-item pull-right"><i class="icon-join"></i></button>
+			</div>
+		</div>
+	{{/each}}
+	{{#unless this.line_items}}
+		<div id="line_item_1" data-id="{{_id}}" class="line-item caldera-config-group">
+			<label><?php _e( 'Line Item', 'caldera-forms-civicrm' ); ?></label>
+			<div class="caldera-config-field">
+				<input type="text" class="block-input field-config magic-tag-enabled caldera-field-bind" name="{{../_name}}[line_items][line_item_1]" value="">
+				<button type="button" class="button remove-line-item pull-right"><i class="icon-join"></i></button>
+			</div>
+		</div>
+	{{/unless}}
+</div>
+
+<script type="text/html" id="line-item-tmpl">
+	<div id="line_item_{{item_id}}" data-id="{{_id}}" class="line-item caldera-config-group">
+		<label><?php _e( 'Line Item', 'caldera-forms-civicrm' ); ?></label>
+		<div class="caldera-config-field">
+			<input type="text" exclude="system" class="block-input field-config magic-tag-enabled caldera-field-bind" name="{{_name}}[line_items][line_item_{{item_id}}]" value="">
+			<button type="button" class="button remove-line-item pull-right"><i class="icon-join"></i></button>
+		</div>
+	</div>
+</script>
+
 <script>
-var cfc_line_items;
 
+	var cfc_add_line_item = function( obj ) {
+		
+		var id = obj.trigger.data('id'),
+		config = JSON.parse( $( '#' + id + ' .processor_config_string' ).val() ),
+		item_id = 'line_item_' + ( $( '#' + id + ' .line-item' ).length + 1 ),
+		_name = 'config[processors][' + id + '][config]';
 
-jQuery( function( $ ){
-	function get_base_form(){
-		return $('.caldera-forms-options-form').formJSON();
+		if ( config.line_items ) {
+			config.line_items[item_id] = '';
+		} else {
+			config['line_items'] = {}
+			config.line_items['line_item_1'] = '';
+		}
+		
+		var processor = { id: id, config: config };
+
+		var line_item = { item_id: item_id, _name: _name };
+		return line_item;
 	}
-	console.log( get_base_form() );
 
-});
+	// FIXME
+	// use this to modify appended element,
+	// can't figure out how to change the 'context' in the handlebars template
+	function line_item_built( obj ) {
+
+		var id = $( '#line_item_' ).data( 'id' ),
+		item_id = 'line_item_' + ( $( '#' + id + '_line_items_wrapper .line-item' ).length ),
+		name = 'config[processors][' + id + '][config][line_items][' + item_id + ']';
+
+		// update name
+		$( '.' + id + '_line_items #line_item_ .field-config' ).attr( 'name', name );
+		// update id
+		$( '.' + id + '_line_items #line_item_' ).attr( 'id', item_id );
+	}
+
+	jQuery( document ).ready( function( $ ) {
+		
+		setTimeout( function(){
+			$( '.line-item .field-config' ).closest( 'span' ).css( 'width', '80%' );
+		}, 3000 )
+		
+		$( '.caldera-editor-body' ).on( 'click', '.remove-line-item', function( e ) {
+			e.preventDefault();
+			$(this).closest( '.line-item' ).remove();
+		} );
+	} )
 
 </script>
