f994cc84940adf72286bf198f73aec149c3ec410
Andrei Mondoc <andreimondoc@gmail.com>
2019-07-30 19:12:30 +0100

    add contact to group at 'processor' stage

--- processors/group/class-group-processor.php
+++ processors/group/class-group-processor.php
@@ -63,7 +63,7 @@ class CiviCRM_Caldera_Forms_Group_Processor {
 			'description' => __( 'Add CiviCRM contact to group', 'cf-civicrm' ),
 			'author' => 'Andrei Mondoc',
 			'template' => CF_CIVICRM_INTEGRATION_PATH . 'processors/group/group_config.php',
-			'pre_processor' => [ $this, 'pre_processor' ],
+			'processor' => [ $this, 'processor' ],
 		];
 
 		return $processors;
@@ -78,7 +78,7 @@ class CiviCRM_Caldera_Forms_Group_Processor {
 	 * @param array $config Processor configuration
 	 * @param array $form Form configuration
 	 */
-	public function pre_processor( $config, $form ) {
+	public function processor( $config, $form ) {
 
 		// cfc transient object
 		$transient = $this->plugin->transient->get();
@@ -92,8 +92,11 @@ class CiviCRM_Caldera_Forms_Group_Processor {
 				'contact_id' => $transient->contacts->{$this->contact_link}, // Contact ID set in Contact Processor
 			] );
 		} catch ( CiviCRM_API3_Exception $e ) {
+			global $transdata;
 			$error = $e->getMessage() . '<br><br><pre>' . $e->getTraceAsString() . '</pre>';
-			return [ 'note' => $error, 'type' => 'error' ];
+			$transdata['error'] = true;
+			$transdata['note'] = $error;
+			return;
 		}
 
 	}
