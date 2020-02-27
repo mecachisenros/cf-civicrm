bcf5606cca3748d1cf629059f9c3cd7acd06410d
Justin Freeman <justin@agileware.com.au>
2020-02-27 16:50:06 +1100

    CMPHSE-33 Allow up to 30 CiviCRM Contacts.

--- includes/class-civicrm-caldera-forms-helper.php
+++ includes/class-civicrm-caldera-forms-helper.php
@@ -98,23 +98,14 @@ class CiviCRM_Caldera_Forms_Helper {
 	public function contact_link_field() {
 
 		ob_start();
-		?>
-				<select class="block-input field-config" name="{{_name}}[contact_link]">
-					<option value="1" {{#is contact_link value=1}}selected="selected"{{/is}}><?php _e( 'Contact 1', 'cf-civicrm' ); ?></option>
-					<option value="2" {{#is contact_link value=2}}selected="selected"{{/is}}><?php _e( 'Contact 2', 'cf-civicrm' ); ?></option>
-					<option value="3" {{#is contact_link value=3}}selected="selected"{{/is}}><?php _e( 'Contact 3', 'cf-civicrm' ); ?></option>
-					<option value="4" {{#is contact_link value=4}}selected="selected"{{/is}}><?php _e( 'Contact 4', 'cf-civicrm' ); ?></option>
-					<option value="5" {{#is contact_link value=5}}selected="selected"{{/is}}><?php _e( 'Contact 5', 'cf-civicrm' ); ?></option>
-					<option value="6" {{#is contact_link value=6}}selected="selected"{{/is}}><?php _e( 'Contact 6', 'cf-civicrm' ); ?></option>
-					<option value="7" {{#is contact_link value=7}}selected="selected"{{/is}}><?php _e( 'Contact 7', 'cf-civicrm' ); ?></option>
-					<option value="8" {{#is contact_link value=8}}selected="selected"{{/is}}><?php _e( 'Contact 8', 'cf-civicrm' ); ?></option>
-					<option value="9" {{#is contact_link value=9}}selected="selected"{{/is}}><?php _e( 'Contact 9', 'cf-civicrm' ); ?></option>
-					<option value="10" {{#is contact_link value=10}}selected="selected"{{/is}}><?php _e( 'Contact 10', 'cf-civicrm' ); ?></option>
-				</select>
-		<?php
+		$maxcontacts = caldera_forms_civicrm()->maxcontacts;
+		echo '<select class="block-input field-config" name="{{_name}}[contact_link]">';
+		for ($count = 1; $count <= $maxcontacts; $count++) {
+            echo '<option value="' . $count . '" {{#is contact_link value=' . $count . '}}selected="selected"{{/is}}>' . sprintf(__('Contact %d', 'cf-civicrm'), $count) . '</option>';
+		}
+		echo '</select>';
 		$contact_link = ob_get_contents();
 		return $contact_link;
-
 	}
 
 	/**
--- processors/relationship/relationship_config.php
+++ processors/relationship/relationship_config.php
@@ -29,16 +29,12 @@ $relationshipPermissions = array(
 	<label><?php _e( 'Contact A', 'cf-civicrm' ); ?></label>
 	<div class="caldera-config-field">
 		<select class="block-input field-config" name="{{_name}}[contact_a]">
-			<option value="1" {{#is contact_a value=1}}selected="selected"{{/is}}><?php _e( 'Contact 1', 'cf-civicrm' ); ?></option>
-			<option value="2" {{#is contact_a value=2}}selected="selected"{{/is}}><?php _e( 'Contact 2', 'cf-civicrm' ); ?></option>
-			<option value="3" {{#is contact_a value=3}}selected="selected"{{/is}}><?php _e( 'Contact 3', 'cf-civicrm' ); ?></option>
-			<option value="4" {{#is contact_a value=4}}selected="selected"{{/is}}><?php _e( 'Contact 4', 'cf-civicrm' ); ?></option>
-			<option value="5" {{#is contact_a value=5}}selected="selected"{{/is}}><?php _e( 'Contact 5', 'cf-civicrm' ); ?></option>
-			<option value="6" {{#is contact_a value=6}}selected="selected"{{/is}}><?php _e( 'Contact 6', 'cf-civicrm' ); ?></option>
-			<option value="7" {{#is contact_a value=7}}selected="selected"{{/is}}><?php _e( 'Contact 7', 'cf-civicrm' ); ?></option>
-			<option value="8" {{#is contact_a value=8}}selected="selected"{{/is}}><?php _e( 'Contact 8', 'cf-civicrm' ); ?></option>
-			<option value="9" {{#is contact_a value=9}}selected="selected"{{/is}}><?php _e( 'Contact 9', 'cf-civicrm' ); ?></option>
-			<option value="10" {{#is contact_a value=10}}selected="selected"{{/is}}><?php _e( 'Contact 10', 'cf-civicrm' ); ?></option>
+			<?php
+			$maxcontacts = caldera_forms_civicrm()->maxcontacts;
+			for ($count = 1; $count <= $maxcontacts; $count++) {
+				echo '<option value="' . $count . '" {{#is contact_a value='. $count . '}}selected="selected"{{/is}}>' . sprintf(__('Contact %d', 'cf-civicrm'), $count) . '</option>';
+			}
+			?>
 		</select>
 	</div>
 </div>
@@ -47,16 +43,12 @@ $relationshipPermissions = array(
 	<label><?php _e( 'Contact B', 'cf-civicrm' ); ?></label>
 	<div class="caldera-config-field">
 		<select class="block-input field-config" name="{{_name}}[contact_b]">
-			<option value="1" {{#is contact_b value=1}}selected="selected"{{/is}}><?php _e( 'Contact 1', 'cf-civicrm' ); ?></option>
-			<option value="2" {{#is contact_b value=2}}selected="selected"{{/is}}><?php _e( 'Contact 2', 'cf-civicrm' ); ?></option>
-			<option value="3" {{#is contact_b value=3}}selected="selected"{{/is}}><?php _e( 'Contact 3', 'cf-civicrm' ); ?></option>
-			<option value="4" {{#is contact_b value=4}}selected="selected"{{/is}}><?php _e( 'Contact 4', 'cf-civicrm' ); ?></option>
-			<option value="5" {{#is contact_b value=5}}selected="selected"{{/is}}><?php _e( 'Contact 5', 'cf-civicrm' ); ?></option>
-			<option value="6" {{#is contact_b value=6}}selected="selected"{{/is}}><?php _e( 'Contact 6', 'cf-civicrm' ); ?></option>
-			<option value="7" {{#is contact_b value=7}}selected="selected"{{/is}}><?php _e( 'Contact 7', 'cf-civicrm' ); ?></option>
-			<option value="8" {{#is contact_b value=8}}selected="selected"{{/is}}><?php _e( 'Contact 8', 'cf-civicrm' ); ?></option>
-			<option value="9" {{#is contact_b value=9}}selected="selected"{{/is}}><?php _e( 'Contact 9', 'cf-civicrm' ); ?></option>
-			<option value="10" {{#is contact_b value=10}}selected="selected"{{/is}}><?php _e( 'Contact 10', 'cf-civicrm' ); ?></option>
+			<?php
+			$maxcontacts = caldera_forms_civicrm()->maxcontacts;
+			for ($count = 1; $count <= $maxcontacts; $count++) {
+				echo '<option value="' . $count . '" {{#is contact_b value='. $count . '}}selected="selected"{{/is}}>' . sprintf(__('Contact %d', 'cf-civicrm'), $count) . '</option>';
+			}
+			?>
 		</select>
 	</div>
 </div>
