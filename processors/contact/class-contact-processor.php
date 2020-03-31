d0421eec089095aef07362971b81606351fdf855
Andrei Mondoc <andreimondoc@gmail.com>
2020-03-31 23:25:19 +0100

    current_employer dedupes, force employer_id for reference field

--- processors/contact/class-contact-processor.php
+++ processors/contact/class-contact-processor.php
@@ -201,11 +201,14 @@ class CiviCRM_Caldera_Forms_Contact_Processor {
 			}
 
 			// contact reference field for organization maps to an array [ 'organization_name' => <name>, 'employer_id' => <id> ]
-			if ( ! empty( $form_values['civicrm_contact']['current_employer'] ) && is_array( $form_values['civicrm_contact']['current_employer'] ) ) {
-				$org = $form_values['civicrm_contact']['current_employer'];
-				$form_values['civicrm_contact']['current_employer'] = $org['organization_name'];
-				// need to be set in case of duplicate orgs with same name
-				$form_values['civicrm_contact']['employer_id'] = $org['employer_id'];
+			if (
+				! empty( $form_values['civicrm_contact']['current_employer'] )
+				&& is_array( $form_values['civicrm_contact']['current_employer'] )
+				&& ! empty( $form_values['civicrm_contact']['current_employer']['employer_id'] )
+			) {
+				$form_values['civicrm_contact']['employer_id'] = $form_values['civicrm_contact']['current_employer']['employer_id'];
+				// need to remove current_employer otherwise Civi will do a dedupe on the organization name
+				unset( $form_values['civicrm_contact']['current_employer'] );
 			}
 
 			/**
