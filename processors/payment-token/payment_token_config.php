e46b08cc2e40704539faba24337393c13640c6f7
Francis Whittle <francis@agileware.com.au>
2020-04-09 15:27:20 +1000

    CFC-56: Add fields descriptions to payment token config

--- processors/payment-token/payment_token_config.php
+++ processors/payment-token/payment_token_config.php
@@ -21,7 +21,7 @@ $pp_result = civicrm_api3( 'PaymentProcessor',
     <label><?php _e( 'Link to', 'cf-civicrm' ); ?></label>
     <div class="caldera-config-field">
 		<?php caldera_forms_civicrm()->helper->contact_link_field() ?>
-        <p><?php _e( 'Select which contact you want to link this processor to.', 'cf-civicrm' ); ?></p>
+        <p class="description"><?php _e( 'Select which contact you want to link this processor to.', 'cf-civicrm' ); ?></p>
     </div>
 </div>
 
@@ -29,22 +29,21 @@ $pp_result = civicrm_api3( 'PaymentProcessor',
     <label for="{{_id}}_is_delete">Delete token</label>
     <div class="caldera-config-field">
         <input id="{{_id}}_is_delete" type="checkbox" class="field-config" name="{{_name}}[is_delete]" value="1" {{#if is_delete}}checked="checked"{{/if}}>
+        <p class="description"> If you want to delete a token, just set the token id. </p>
     </div>
-    <p class="description">
-        If you want to delete a token, just set the token id.
-    </p>
 </div>
 
 <?php
 foreach ( $tokenFields['values'] as $key => $value ) {
 	if ( ! in_array( $value['name'], $excludedFields ) && $value['required'] ) { ?>
-        <div id="{{_id}}_<?php echo $value['name']; ?>" class="caldera-config-group">
-            <label><?php echo __( $value['title'] ); ?> </label>
-			<?php
-			switch ( $key ) {
+        <div class="caldera-config-group">
+            <label for="{{_id}}_<?php echo $value['name']; ?>"><?php echo __( $value['title'] ); ?></label>
+            <div class="caldera-config-field">
+			    <?php
+			    switch ( $key ) {
 				case 'payment_processor_id':
 					?>
-                    <select class="block-input field-config" name="{{_name}}[<?php echo $key ?>]" required>
+                    <select id="{{_id}}_<?php echo $value['name']; ?>" class="block-input field-config" name="{{_name}}[<?php echo $key ?>]" required>
 						<?php foreach ( $pp_result['values'] as $pp_key => $pp_value ) { ?>
                             <option value="<?php echo esc_attr( $pp_value['id'] ); ?>"
                                     {{#is payment_processor_id value=<?php echo $pp_value['id']; ?>}}selected="selected"{{/is}}>
@@ -56,35 +55,34 @@ foreach ( $tokenFields['values'] as $key => $value ) {
 					break;
 				default:
 					?>
-                    <div class="caldera-config-field">
-                        <input
-                                type="text" id="{{_id}}_name"
-                                class="block-input field-config magic-tag-enabled caldera-field-bind"
-                                name="{{_name}}[<?php echo $key; ?>]"
-                                value="{{<?php echo $key; ?>}}">
-                    </div>
+                        <input id="{{_id}}_<?php echo $value['name']; ?>" type="text" id="{{_id}}_name"
+                               class="block-input field-config magic-tag-enabled caldera-field-bind"
+                               name="{{_name}}[<?php echo $key; ?>]"
+                               value="{{<?php echo $key; ?>}}">
 				<?php } ?>
+                <p class="description"><?php print $value['description'] ?></p>
+            </div>
         </div>
 		<?php
 	}
 } ?>
 <hr style="clear: both;"/>
 
-<h2 style="display: inline-block;">Optional Fields</h2>
+<h2 style="display: inline-block;"><?php _e('Optional Fields', 'cf-civicrm'); ?></h2>
 <?php
 foreach ( $tokenFields['values'] as $key => $value ) {
 	if ( ! in_array( $value['name'], $excludedFields ) && ! $value['required'] ) {
 		switch ( $key ) {
 			default:
 				?>
-                <div id="{{_id}}_<?php echo $value['name']; ?>" class="caldera-config-group">
-                    <label><?php echo __( $value['title'] ); ?> </label>
+                <div class="caldera-config-group">
+                    <label for="{{_id}}_<?php echo $value['name']; ?>"><?php echo __( $value['title'] ); ?> </label>
                     <div class="caldera-config-field">
-                        <input
-                                type="text" id="{{_id}}_name"
-                                class="block-input field-config magic-tag-enabled caldera-field-bind"
-                                name="{{_name}}[<?php echo $key; ?>]"
-                                value="{{<?php echo $key; ?>}}">
+                        <input type="text" id="{{_id}}_<?php echo $value['name']; ?>"
+                               class="block-input field-config magic-tag-enabled caldera-field-bind"
+                               name="{{_name}}[<?php echo $key; ?>]"
+                               value="{{<?php echo $key; ?>}}">
+                        <p class="description" ><?php print $value['description'] ?></p>
                     </div>
                 </div>
 			<?php
